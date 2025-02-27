4e9459bdb81d595a7236a6c4765c7d8ddcf0d60b
Aisha Hassan <aishahassan669912@gmail.com>
2025-02-27 10:02:55 -0800

    review-string

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define the currentUser variable
+const currentUser = 'Grace Hopper';
+
+// Define welcomeMessage with the currentUser value
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define excitedWelcomeMessage in uppercase
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define shortGreeting with only the first letter of currentUser
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
